b861b7ec2f0c0168c5f934796cf398b0c35b773a
catarim <126827483+catarim@users.noreply.github.com>
2026-04-17 18:15:06 -0300

    Update install.js

--- install.js
+++ install.js
@@ -1,28 +1,37 @@
 module.exports = {
   run: [
-    // 1. Clonar, Criar ambiente e Instalar tudo em um único comando shell para evitar que o terminal feche
     {
       method: "shell.run",
       params: {
-        message: [
-          "git clone https://github.com/OpenTalker/video-retalking app",
-          "cd app",
-          "pip install -r requirements.txt",
-          "mkdir checkpoints"
-        ]
-      }
-    },
-    // 2. Baixar os modelos (Checkpoints) - Esses precisam de tempo, então deixamos separados
-    {
-      method: "shell.run",
-      params: {
-        message: "curl -L -o app/checkpoints/main_gfvgan.pth https://huggingface.co/pbeast/video-retalking/resolve/main/main_gfvgan.pth"
+        message: "git clone https://github.com/OpenTalker/video-retalking app"
       }
     },
     {
       method: "shell.run",
       params: {
-        message: "curl -L -o app/checkpoints/L16_96.pth https://huggingface.co/pbeast/video-retalking/resolve/main/L16_96.pth"
+        message: "pip install -r requirements.txt",
+        path: "app"
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        message: "mkdir checkpoints",
+        path: "app"
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        message: "curl -L -o checkpoints/main_gfvgan.pth https://huggingface.co/pbeast/video-retalking/resolve/main/main_gfvgan.pth",
+        path: "app"
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        message: "curl -L -o checkpoints/L16_96.pth https://huggingface.co/pbeast/video-retalking/resolve/main/L16_96.pth",
+        path: "app"
       }
     }
   ]
